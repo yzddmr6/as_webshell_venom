8800faa7a9606829a68d62093c31765eb996cf34
Mr6 <46088090+yzddmr6@users.noreply.github.com>
2019-06-04 16:44:40 +0800

    Add files via upload

---
+++ language/en.js
@@ -0,0 +1,23 @@
+/**
+ * en_US
+ */
+
+module.exports = {
+    title: "Generate Shell",
+    toolbar: {
+        new: "Generate",
+        clear: "Clear",
+        copy: "Copy"
+    },
+    pwd_hint: "Delete this line, pwd",
+    message: {
+        edit_null_value: "Copy fail, content is empty",
+        copy_success: "Copy success",
+        copy_fail: "Copy fail",
+        pwd_invalid: "The password must be alphabetic and numeric",
+        gen_success: "Generate success"
+    },
+    prompt: {
+        create_pwd: "Your password. default random passwords"
+    }
+};
---
+++ language/zh.js
@@ -0,0 +1,23 @@
+/**
+ * zh_CN
+ */
+
+module.exports = {
+    title: "生成Shell",
+    toolbar: {
+        new: "生成",
+        clear: "清空",
+        copy: "复制"
+    },
+    pwd_hint: "使用时请删除此行, 连接密码",
+    message: {
+        edit_null_value: "复制失败,内容为空",
+        copy_success: "复制成功",
+        copy_fail: "复制失败",
+        pwd_invalid: "密码必须为字母、数字",
+        gen_success: "生成成功"
+    },
+    prompt: {
+        create_pwd: "自定义密码,直接点确定会生成随机密码"
+    }
+};
